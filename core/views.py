dd27d6499f24b6103033b41edc8b00067619b1dc
Yorrmungand <edgarmarques@hotmail.com.br>
2019-05-14 23:18:45 -0300

    Added templates

--- core/views.py
+++ core/views.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Post
 
 # Create your views here.
+
+
+class HomeView(TemplateView):
+    template_name = "index.html"
